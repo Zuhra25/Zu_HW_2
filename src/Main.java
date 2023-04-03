fc951835219a9facc72e15eebab82eb74ebf7759
ZU <zshaihova@gmail.com>
2023-04-03 19:45:54 +0300

    задача 1,2,3,4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
+
     }
 
     public static void task1 () {
@@ -40,5 +42,13 @@ public class Main {
         System.out.println(paper);
     }
 
-
+    public static void task4 () {
+        System.out.println("Задача 4:");
+        var friend  = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+    }
 }
