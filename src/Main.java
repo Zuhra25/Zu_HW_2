937516039d558d6c3e6469f98c81a487ad3dbff6
ZU <zshaihova@gmail.com>
2023-04-03 19:48:18 +0300

    задача 1,2,3,4,5

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
-
+        task5();
     }
 
     public static void task1 () {
@@ -51,4 +51,18 @@ public class Main {
         friend = friend / 7;
         System.out.println(friend);
     }
+
+    public static void task5 () {
+        System.out.println("Задача 5:");
+        var frog   = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+    }
+
+
 }
