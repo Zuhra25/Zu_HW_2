bbb9f1e1af278f601efba9dec615939af3f5d236
ZU <zshaihova@gmail.com>
2023-04-03 19:35:25 +0300

    задача 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1 () {
-        System.out.println("Задача 1");
+        System.out.println("Задача 1:");
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
@@ -15,7 +16,7 @@ public class Main {
     }
 
     public static void task2 () {
-        System.out.println("Задача 2");
+        System.out.println("Задача 2:");
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
@@ -26,4 +27,18 @@ public class Main {
         System.out.println(cat);
         System.out.println(paper);
     }
+    public static void task3 () {
+        System.out.println("Задача 3:");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+
+
 }
