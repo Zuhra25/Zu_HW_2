8c053e6583f706f651087ea3bcc578c66f6b51a2
ZU <zshaihova@gmail.com>
2023-04-03 19:33:03 +0300

    задача 1,2

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,14 @@ public class Main {
 
     public static void task2 () {
         System.out.println("Задача 2");
-        // Пишем код для задачи 2
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
